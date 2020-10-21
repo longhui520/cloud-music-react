8f179b4edbf4ebe8487c56ee2445ba556fce67d9
longhui <625243422@qq.com>
2020-10-21 18:08:20 +0800

    更新

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,8 +1,26 @@
-import React from  'react'
-
-function Album(){
+import React from 'react'
+import { Container } from './style'
+import { CSSTransition } from 'react-transition-group'
+import  Header  from './../../baseUI/header/index';
+function Album(props) {
+    const [showState, setShowState] = React.useState(true)
+    const handleBack = () => {
+        setShowState (false);
+    };
     return (
-        <div>Album</div>
+        <CSSTransition
+            in={showState}
+            timeout={200}
+            classNames="fly"
+            unmountOnExit
+            appear={true} 
+            onExited={props.history.goBack}
+        >
+            <Container>
+                <Header title={"返回"} handleClick={handleBack}></Header>
+            </Container>
+
+        </CSSTransition>
     )
 }
 export default React.memo(Album)
